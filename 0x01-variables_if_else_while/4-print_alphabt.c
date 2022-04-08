be690bc34db70c01240fcb37c208b2f1302fe7d0
Mohammed <mohejemal12@gmail.com>
2022-04-08 04:02:11 -0700

    Alphabet with out q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - printing a-z and A-Z
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char alps = 'a';
+	char alpC = 'A';
+
+	while (alps < 'z' + 1)
+	{
+		if (alps != 'q' && alps != 'e')
+			putchar(alps);
+		alps++;
+	}
+	putchar('\n');
+	return (0);
+}
