0edb8782256a12239eaeff795b3095435f3df97f
Mohammed <mohejemal12@gmail.com>
2022-04-08 04:30:42 -0700

    NumberZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing 0-9
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	for (int i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
