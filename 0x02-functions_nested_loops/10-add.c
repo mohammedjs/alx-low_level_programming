0af76ee649be3c62092fd885f279d1107fa4560b
Mohammed <mohejemal12@gmail.com>
2022-04-13 05:21:03 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - adding two numbers inputted from keyboard
+ *
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	int num1, num2, sum;
+
+	sum = num1 + num2;
+
+	printf("%d\n", sum)
+
+	return (0);
+}
