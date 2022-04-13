87bca6105046258a590a901e193ef67d498b4b7b
Mohammed <mohejemal12@gmail.com>
2022-04-13 05:55:31 -0700

    time table n 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * Prinyt_times_table -print the n times table
+ *
+ * @n:numbesr tiems table (0 < n <= 15)
+ *
+ * Return: no return
+ */
+void print_times_table(int n)
+{
+	int a, b, opr;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
+				opr = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (opr <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (opr <= 99)
+				{
+					_putchar(32);
+					_putchar((opr / 10) + 48);
+					_putchar((opr % 10) + 48);
+				}
+				else
+				{
+					_putchar(((opr / 100) % 10) + 48);
+					_putchar(((opr / 10) % 10) + 48);
+					_putchar((opr % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
