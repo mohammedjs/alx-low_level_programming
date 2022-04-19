7488dba985b5b38513d467feadf982265bd3ad7a
Mohammed <mohejemal12@gmail.com>
2022-04-18 18:52:33 -0700

    adding main

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char);
+void reset_to_98(int *n);
+void swap_int(int *, int *);
+void _strlen(char *);
+void _puts(char *);
+void print_rev(char *);
+void rev_string(char *);
+void puts2(char *);
+void puts_half(char *);
+void print_array(int *, int);
+char * _strcpy(char *, char *);
+int _atoi(char *);
