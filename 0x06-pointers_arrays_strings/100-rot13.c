40998c9f924f6ae82837e7f0bdea23e5de912b9d
Mohammed Jemal <mohejemal12@gmail.com>
2022-04-22 01:13:17 +0000

    adding 100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,35 +1,31 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * rot13- encodes a string using rot13
+ * @s: string to encode
+ * Return: encoded string
  */
-void print_number(int n)
+char *rot13(char *s)
 {
-	unsigned int m, d, count;
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M'};
 
-	if (n < 0)
-	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
-	}
+	int i, j;
 
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		d /= 10;
-		count *= 10;
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i])
+			i++;
+		if (s[j] == a[i])
+			s[j] = b[i];
 	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	return (s);
 }
