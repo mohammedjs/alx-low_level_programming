3310c022cd15d2be1cc4dad316d2d4a7e0b78ce2
Mohammed Jemal <mohejemal12@gmail.com>
2022-04-22 01:19:10 +0000

    adding print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+include "main.h"
+
+/**
+ * print_number- prints a number
+ * @n: integer
+ * Return: void
+ * */
+
+void print_number(int n)
+{
+	unsigned int max, d, count;
+	if (n < 0)
+	{
+		_putchar(45);
+		max = n * -1;
+	}
+
+	else
+	{
+		max = n;
+	}
+
+	d = max;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((max / count) % 10) + 48);
+	}
+}
