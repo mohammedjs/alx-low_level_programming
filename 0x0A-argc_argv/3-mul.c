be3baae6b3d202bead8375c2a594ce8d138f2796
Mohammed <mohejemal12@gmail.com>
2022-05-04 19:58:25 -0700

    adding 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array containing program command line
+ * Return: 0 sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
